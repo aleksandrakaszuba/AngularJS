222a1eb8597f40f080ffaa4de2e4ff3dc2d4f430
aleksandrakaszuba <ola.kaszuba@gmail.com>
2017-11-28 19:36:30 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+(function(){
+	'use strict';
+
+	var myApp = angular.module("myApp",['ngRoute'])
+    myApp.config(function($routeProvider, $locationProvider) {
+        $routeProvider
+       
+             .when("/", {
+                templateUrl: "partials/intro.html"
+            })
+            .when("/installation", {
+                templateUrl: "partials/installation.html"
+            })
+            .when("/tutorial", {
+                templateUrl: "partials/tutorial.html"
+            })
+            .when("/conclusion", {
+                templateUrl: "partials/conclusion.html"
+            })
+            .when("/credits", {
+                templateUrl: "partials/credits.html"
+            });
+             //HTML5 History API
+   $locationProvider.html5Mode({ enabled: true, requireBase: true }).hashPrefix('!');
+    });
+
+
+})()
